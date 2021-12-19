da6844960076eb726989c2606d139d86b2056215
Lloyd Hamilton <lloydhamilton1991@gmail.com>
2021-12-19 16:35:10 +0000

    added predict.py to test localhost docker image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.9-slim
-COPY ./models/rf_clf.joblib /deploy/
-COPY ./scripts/flask_api.py /deploy/
-COPY poetry.lock pyproject.toml /deploy/
-WORKDIR /deploy/
 ENV YOUR_ENV=${YOUR_ENV} POETRY_VERSION=1.1.11
 RUN pip install "poetry==${POETRY_VERSION}"
+WORKDIR /deploy
+COPY poetry.lock pyproject.toml /deploy/
 RUN poetry config virtualenvs.create false
 RUN poetry install
+COPY ./models/rf_clf.joblib /deploy
+COPY ./scripts/flask_api.py /deploy
 EXPOSE 8080
 ENTRYPOINT ["python", "flask_api.py"]
---
+++ scripts/predict.py
@@ -0,0 +1,26 @@
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+import numpy as np
+import requests
+import json
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+url = 'http://localhost:8080/predict'
+headers = {
+    'Content-type': "application/json"
+}
+
+data = json.dumps(X_test.tolist())
+response = requests.post(url, headers=headers, data=data)
+predictions = np.array(json.loads(response.text))
+print(f'Predictions: {predictions}')
+print(f'Accuracy Score: {accuracy_score(predictions, y_test)}')
