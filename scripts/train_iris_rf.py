c7ea82677066061a14ba07f99e3b04df968f87a5
Lloyd Hamilton <lloydhamilton1991@gmail.com>
2021-12-24 17:10:50 +0000

    Moved display_score function to start of script

--- scripts/train_iris_rf.py
+++ scripts/train_iris_rf.py
@@ -4,14 +4,14 @@ from sklearn.model_selection import cross_val_score
 import joblib
 from sklearn.datasets import load_iris
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-
 def display_scores(scores):
     print("Scores:", scores)
     print("Means:", scores.mean())
     print("Standard Deviation:", scores.std())
+    
+iris = load_iris()
+X = iris.data
+y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
