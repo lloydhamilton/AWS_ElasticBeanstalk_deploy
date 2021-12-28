5b44c776844f5b040fdc1c9bcfe1b523271f7be7
Lloyd Hamilton <lloydhamilton1991@gmail.com>
2021-12-28 14:37:42 +0000

    updated scripts as executable functions

--- scripts/eb_predict.py
+++ scripts/eb_predict.py
@@ -1,25 +1,29 @@
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
-import numpy as np
-import requests
-import json
+def eb_predict():
+    from sklearn.metrics import accuracy_score
+    from sklearn.model_selection import train_test_split
+    from sklearn.datasets import load_iris
+    import numpy as np
+    import requests
+    import json
 
-iris = load_iris()
-X = iris.data
-y = iris.target
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-url = 'http://flask-api-dev.eu-west-2.elasticbeanstalk.com/predict'
-headers = {
-    'Content-type': "application/json"
-}
+    url = 'http://flask-api-dev.eu-west-2.elasticbeanstalk.com/predict'
+    headers = {
+        'Content-type': "application/json"
+    }
 
-data = json.dumps(X_test.tolist())
-response = requests.post(url, headers=headers, data=data)
-predictions = np.array(json.loads(response.text))
-print(f'Predictions: {predictions}')
-print(f'Accuracy Score: {accuracy_score(predictions, y_test)}')
+    data = json.dumps(X_test.tolist())
+    response = requests.post(url, headers=headers, data=data)
+    predictions = np.array(json.loads(response.text))
+    print(f'Predictions: {predictions}')
+    print(f'Accuracy Score: {accuracy_score(predictions, y_test)}')
+
+if __name__ == '__main__':
+    eb_predict()
--- scripts/train_iris_rf.py
+++ scripts/train_iris_rf.py
@@ -1,24 +1,28 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-import joblib
-from sklearn.datasets import load_iris
+def train_rf():
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.model_selection import cross_val_score
+    import joblib
+    from sklearn.datasets import load_iris
 
-def display_scores(scores):
-    print("Scores:", scores)
-    print("Means:", scores.mean())
-    print("Standard Deviation:", scores.std())
-    
-iris = load_iris()
-X = iris.data
-y = iris.target
+    def display_scores(scores):
+        print("Scores:", scores)
+        print("Means:", scores.mean())
+        print("Standard Deviation:", scores.std())
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
 
-rf_clf = RandomForestClassifier()
-rf_clf.fit(X_train, y_train)
-rf_scores = cross_val_score(rf_clf, X_train, y_train, scoring = 'accuracy', cv = 3)
-display_scores(rf_scores)
-joblib.dump(rf_clf, 'models/rf_clf.joblib')
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    rf_clf = RandomForestClassifier()
+    rf_clf.fit(X_train, y_train)
+    rf_scores = cross_val_score(rf_clf, X_train, y_train, scoring = 'accuracy', cv = 3)
+    display_scores(rf_scores)
+    joblib.dump(rf_clf, 'models/rf_clf.joblib')
+
+if __name__ == '__main__':
+    train_rf()
